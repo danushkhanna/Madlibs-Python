3559bc8abc9d1d383526811abb1e533bc0e43d7a
danushkhanna <113158564+danushkhanna@users.noreply.github.com>
2022-11-26 11:59:37 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+youtuber="danush khanna"
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
+print("subscribe to " + youtuber)
+
+adj=input("adjective: ")
+verb1=input("verb: ")
+verb2=input("verb: ")
+famous_person=input("famous person: ")
+
+madlib = f"computer programming is so {adj}! it makes me so excited all the time because \
+i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
+
+
